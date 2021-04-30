82974c7741cbfa506241784961f2acb1aae1576f
Rabbit0w0 <401395069@qq.com>
2021-04-30 20:50:13 +0800

    init bungee

---
+++ README.md
@@ -0,0 +1,12 @@
+BungeeCord
+==========
+Layer 7 proxy designed to link Minecraft servers.
+--------------------------------------------------
+
+BungeeCord is a sophisticated proxy and API designed mainly to teleport players between multiple Minecraft servers. It is the latest incarnation of similar software written by the author from 2011-present.
+
+How to compile?
+----------------
+`mvn clean install`
+
+(c) 2012-2021 SpigotMC Pty. Ltd.
---
+++ proxy/src/main/java/net/md_5/bungee/command/CommandVersion.java
@@ -0,0 +1,21 @@
+package net.md_5.bungee.command;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class CommandVersion extends Command
+{
+
+    public CommandVersion()
+    {
+        super( "bungee" );
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args)
+    {
+        sender.sendMessage( ChatColor.GOLD + ChatColor.BOLD.toString() + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() );
+    }
+}
