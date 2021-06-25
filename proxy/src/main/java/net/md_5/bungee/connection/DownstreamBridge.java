6021e58aa2bc2cf262938b8afa20f939d5d0ef32
Rabbit0w0 <rabbit0w0@outlook.com>
2021-06-25 21:09:56 +0800

    Rebrand

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -285,7 +285,7 @@ public class DownstreamBridge extends PacketHandler
             Preconditions.checkState( !serverBrand.contains( bungee.getName() ), "Cannot connect proxy to itself!" );
 
             brand = ByteBufAllocator.DEFAULT.heapBuffer();
-            DefinedPacket.writeString( bungee.getName() + " (" + bungee.getVersion() + ")" + " <- " + serverBrand, brand );
+            DefinedPacket.writeString( bungee.getName() + " <- " + serverBrand, brand );
             pluginMessage.setData( DefinedPacket.toArray( brand ) );
             brand.release();
             // changes in the packet are ignored so we need to send it manually
